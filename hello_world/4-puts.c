940aff76e1d38d0d62c03ffcb9895e1c2e3985e8
micaelapicco <6314@holbertonstudents.com>
2023-02-15 08:45:49 -0800

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-	int main (void)
+/**
+ * main - Puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
 	
-	{puts ("\"Programming is like building a mulilingual puzzle");
-			return (0);
-	}
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
