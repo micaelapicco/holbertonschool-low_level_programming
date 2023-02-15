bb24ee581ccbc8ff6a990bd9e63967a2ca530a0f
micaelapicco <6314@holbertonstudents.com>
2023-02-15 09:17:44 -0800

    fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{	int intType;
+	long int longintType;
+	long long int longlongintType;
+	char charType;
+	float floatType;
+	printf("Size of int: %zu bytes\n",sizeof(intType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of char: %zu bytes", sizeof(charType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	return (0);
+}
