759c434cef983c39a761972edce4fea0e8319cb4
micaelapicco <6314@holbertonstudents.com>
2023-02-17 06:11:17 -0800

    lastdigit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print ld
+ * @ld: number
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int ld)
+{
+	int n
+
+	if (ld)
+	{
+		n = ld % 10
+		_putchar('n');
+	}
+}
