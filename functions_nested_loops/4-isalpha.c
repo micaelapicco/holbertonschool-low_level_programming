4ae9c7958cac06f0fe7fd0578d9a297c3fc60dc9
micaelapicco <6314@holbertonstudents.com>
2023-02-17 04:47:35 -0800

    ilapa

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islapha - alphabetic
+ * _isalpha - alphabetic
  * @c: run
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
