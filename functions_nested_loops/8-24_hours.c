12fe2812b57c16fc68e89eee56bdfa2baf5fbca3
micaelapicco <6314@holbertonstudents.com>
2023-02-17 08:19:25 -0800

    fiz

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,6 +10,8 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
+	int c;
+	int d;
 
 	for (a = 0; a <= 23; a++)
 	{
@@ -17,13 +19,13 @@ void jack_bauer(void)
 		b = a % 10;
 		_putchar(a);
 		_putchar(b);
-	}
-	_putchar(':');
-	for (a = 0; a <= 59; a++)
-	{
-		a = a / 10;
-		b = a % 10;
-		_putchar(a);
-		_putchar(b);
+		_putchar(':');
+		for (c = 0; c <= 59; c++)
+		{
+		c = c / 10;
+		d = c % 10;
+		_putchar(c);
+		_putchar(d);
+		}
 	}
 }
