11d902d67f65d4b445d3069ce3a64f41a3034a2b
micaelapicco <6314@holbertonstudents.com>
2023-02-22 10:02:10 -0800

    fix

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for  (a = '0'; a <= '9'; a++)
+	{
+		if (a == '2' && a == '4')
+		{
+			a++;
+		}
+		_puchar(a + '0');
+	}
+	_putchar('\n');
+}
