8530e78a1da498d573957b0e5f2351f7089a3190
micaelapicco <6314@holbertonstudents.com>
2023-02-23 07:50:00 -0800

    fix

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - fizz buzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0 && n % 3 != 0)
+		{
+			if (n == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else if (n % 3 != 0 || n % 5 != 0)
+		{
+			printf("%d ", n);
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+	}
+	return (0);
+}
