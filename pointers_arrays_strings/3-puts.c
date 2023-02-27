5813b293ed77936c03d0fa4094ea706eb59e2140
micaelapicco <6314@holbertonstudents.com>
2023-02-27 09:13:24 -0800

    print_string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int c;
+
+	for (c = 0; c[str]; c++)
+	{
+	_putchar(c[str]);
+	}
+	_putchar('\n');
+}
