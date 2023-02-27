b34e525ba30e6ab01ef023f36976ee972f973bd5
micaelapicco <6314@holbertonstudents.com>
2023-02-27 08:52:53 -0800

    character

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string character
+ * Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = '0'; s[c] <= '\0'; c++)
+	{
+	}
+	return (c);
+}
