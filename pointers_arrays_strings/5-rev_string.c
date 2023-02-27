b2521a189105cbf01e86779cc8dc74b65f266c8e
micaelapicco <6314@holbertonstudents.com>
2023-02-27 11:04:17 -0800

    string_reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - string
+ * @r: reverse
+ * @c: count
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	int string, right, left;
+
+	for (right = 0; s[right]; right++)
+		;
+	for (left = 0; left < right / 2; left++)
+	{
+		string = s[left];
+		s[left] = s[right - left - 1];
+		s[right - left - 1] = string;
+	}
+}
