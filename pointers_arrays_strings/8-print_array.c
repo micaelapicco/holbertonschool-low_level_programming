f8707e539511a406a5edeed0562ba67af18aa076
micaelapicco <6314@holbertonstudents.com>
2023-02-28 06:58:12 -0800

    save

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print n elements or array
+ * @n: number array
+ * Return: Always 0 (Success)
+ */
+void print_array(int *a, int n)
+{
+		/*position*/
+
+	for (n = 0; n[a]; n++)
+	
+	while (n < n[a])
+	{
+		printf("%d, ", a[n]);
+		n++;
+	}
+	printf("%d", a[n]);
+
+	printf("\n");
+}
