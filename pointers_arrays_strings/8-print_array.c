aa1d242e8bd440a83f1fb7220955ff78c8fd81a3
micaelapicco <6314@holbertonstudents.com>
2023-02-28 08:37:05 -0800

    count_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,20 +3,24 @@
 /**
  * print_array - print n elements or array
  * @n: number array
+ * @a: array
  * Return: Always 0 (Success)
  */
 void print_array(int *a, int n)
 {
-		/*position*/
+	int r;/*position*/
 
-	for (n = 0; n[a]; n++)
-	
-	while (n < n[a])
+	for (r = 0; r < n; r++)
 	{
-		printf("%d, ", a[n]);
-		n++;
+		printf("%d", a[r]);
+		if (r == n - 1)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf(", ");
+		}
 	}
-	printf("%d", a[n]);
-
 	printf("\n");
 }
