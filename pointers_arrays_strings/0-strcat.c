ba6c63433220bcef007e9f8d88e8b235dff780a9
micaelapicco <6314@holbertonstudents.com>
2023-03-01 08:53:16 -0800

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings
+ * @*dest: destination
+ * @src: source
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int d;
+	int s;
+
+	for (d = 0; dest[d] != '\0'; d++)
+	{
+	}
+	for (s = 0; src[s] != '\0'; s++, d++)
+	{
+		dest[d] = src[s];
+	}
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,3 +13,13 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 /* prototypes C - Pointers, arrays and strings*/
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+/* prototypes C - more pointers, arrays and strings */
