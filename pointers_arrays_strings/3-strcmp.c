0d87ad7a7d741a2dcb30b2702e8c7615a8187008
micaelapicco <6314@holbertonstudents.com>
2023-03-02 04:37:05 -0800

    compare_strings

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - compare string
+ * @s1: string one
+ * @s2: string two
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+		return (*s1 - *s2);
+}
