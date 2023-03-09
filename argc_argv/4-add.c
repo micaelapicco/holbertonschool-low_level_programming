a619cd1a627220fa68871dcb35d0554c4b5e2fb2
micaelapicco <6314@holbertonstudents.com>
2023-03-09 03:52:05 -0800

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - Entry point
  * @argc: argument count
@@ -9,19 +10,22 @@
 int main(int argc, char *argv[])
 {
 	int result = 0;
-	int i;
+	int x, y;
 
-	for (i = 1; argv[i]; i++)
+	for (x = 1; argv[x]; x++)
 	{
-	if (argc > 1 && atoi(argv[i]) > 0)
-	{
-		result += atoi(argv[i]);
-	}
-	else
-	{
+		for (y = 0; argv[x][y]; y++)
+		{
+		if (argc > 1 && isdigit(argv[x][y]))
+		{
+		}
+		else
+		{
 		printf("Error\n");
 		return (1);
-	}
+		}
+		}
+		result += atoi(argv[x]);
 	}
 	printf("%d\n", result);
 	return (0);
