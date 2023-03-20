6e4667d5b7412387a7aa1112e8798b9cbddbbb28
micaelapicco <6314@holbertonstudents.com>
2023-03-20 09:07:05 -0700

    print

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,14 +14,14 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 		}
 		else
-		printf("Name: (nil)");
+		printf("Name: (nil\n)");
 
 		if ((*d).age)
 		{
 			printf("Age: %f\n", d->age);
 		}
 		else
-		printf("0000000\n");
+		printf("Age: 0.000000\n");
 
 		if ((*d).owner)
 		{
@@ -29,7 +29,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil\n)");
 		}
 	}
 }
