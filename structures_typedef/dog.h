da1766dabd9d5114bf3500952845c0ded47a490c
micaelapicco <6314@holbertonstudents.com>
2023-03-20 06:21:11 -0700

    structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog information
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: structures dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
