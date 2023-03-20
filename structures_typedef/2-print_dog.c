3d2a4f964803d360f8cf86dfef33b124a082c075
micaelapicco <6314@holbertonstudents.com>
2023-03-20 09:08:51 -0700

    print

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 		}
 		else
-		printf("Name: (nil\n)");
+		printf("Name: (nil)\n");
 
 		if ((*d).age)
 		{
@@ -29,7 +29,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Owner: (nil\n)");
+			printf("Owner: (nil)\n");
 		}
 	}
 }
