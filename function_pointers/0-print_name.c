0873ade46dfb830be27ebac77b4ff775f7e82da1
micaelapicco <6314@holbertonstudents.com>
2023-03-22 04:56:12 -0700

    print_name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - print *name
+ * @name: name
+ * @f: function print name
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
