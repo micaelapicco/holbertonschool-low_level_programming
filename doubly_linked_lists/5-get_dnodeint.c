c4c9baa42bf695bf1815011c45947b5aa1472e3c
micaelapicco <6314@holbertonstudents.com>
2023-04-04 14:08:12 -0700

    fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int num = 0;
 	dlistint_t *p = head;
 
-	if (!head || !index)
+	if (!head)
 		return (NULL);
 
 	for (; num < index; num++)
