357db617620af523e62060ba65583225d8d11ba8
micaelapicco <6314@holbertonstudents.com>
2023-04-06 11:47:30 -0700

    fix

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -7,7 +7,7 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *aux = *head, temp;
+	dlistint_t *aux = *head, *temp;
 	unsigned int i = 0;
 
 	if (aux != NULL)
