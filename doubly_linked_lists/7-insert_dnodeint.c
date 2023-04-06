e473c57474ff4d577a342de3d2567f3491ffe9cb
micaelapicco <6314@holbertonstudents.com>
2023-04-06 08:11:11 -0700

    ready

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 
 	/*check malloc works*/
-	if (!new || idx < 1)
+	if (!new)
 		return (NULL);
 	/*asign value new node*/
 	new->n = n;
