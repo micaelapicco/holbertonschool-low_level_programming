a389189d8a87e436692f5b22fe5cc02d649855ed
micaelapicco <6314@holbertonstudents.com>
2023-04-07 13:27:52 -0700

    dis

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,14 +7,18 @@
  * Return: Nothing.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-{
+{ /*calculate XOR between n and m, this produced 1 if bits are diferents
+	or 0 if bits are the same*/
 	unsigned long int result = n ^ m;
 	unsigned int count = 0;
 
+	/*with loop count number of bits are necessary change, using & 1
+	 * and increse the counter and move one 1 bit using result*/
 	while (result)
 	{
 		count += result & 1;
 		result >>= 1;
 	}
+	/*return the number of bits that fliped to changed n for m*/
 	return (count);
 }
