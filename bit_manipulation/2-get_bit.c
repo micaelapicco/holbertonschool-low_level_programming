5db70960bebe25d68c89241e637645e448652b4e
micaelapicco <6314@holbertonstudents.com>
2023-04-07 12:31:33 -0700

    fix

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -2,7 +2,8 @@
 /**
  * get_bit - function that returns the value of a bit at a given index
  * @index: index of the bit you want to get
- * Return: the value of the vit a index or -1 if an error
+ * @n: number to return
+ * Return: the value of the bit a index or -1 if an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
