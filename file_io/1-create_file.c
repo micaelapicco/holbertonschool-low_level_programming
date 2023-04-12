2417d81158b6737736401c4658b67fcc444c05ac
micaelapicco <6314@holbertonstudents.com>
2023-04-12 09:29:49 -0700

    asdf

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,23 +1,5 @@
 #include "main.h"
 /**
- * _memcpy - copy memory area
- * @n: copy n bytes from
- * @src: source memory
- * @dest: to destination memory area
- * Return: Always 0 (Success)
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int s; /*variable*/
-
-	for (s = 0; s < n; s++)
-	{
-		dest[s] = src[s];
-	}
-
-	return (dest);
-}
-/**
  * create_file - function that creates a file
  * @filename: file
  * @text_content: content of filename
@@ -26,7 +8,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 int create_file(const char *filename, char *text_content)
 {
 	int file, len = 0;
-	/*char *buffer;*/
 
 	if (!filename)
 		return (-1);
@@ -37,20 +18,12 @@ int create_file(const char *filename, char *text_content)
 	}
 	len++;
 
-	/*buffer = malloc(len);
-	if (!buffer)
-		return (-1);
-	if (text_content)
-		_memcpy(buffer, text_content, len);*/
-	
-	/*buffer[len - 1] = '\0';*/
-
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
 	write(file, text_content, len - 1);
 	close(file);
-	/*free(buffer);*/
+
 	return (1);
 	}
