cb5905ce7d95fe42885774cb34c3f39d2c62dc08
Micaela Picco <124269707+micaelapicco@users.noreply.github.com>
2023-05-30 18:25:10 -0300

    Update 9-fizz_buzz.c

--- 04_more_functions_nested_loops/9-fizz_buzz.c
+++ 04_more_functions_nested_loops/9-fizz_buzz.c
@@ -12,28 +12,18 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 != 0)
-		{
 			printf("Fizz ");
-		}
 		else if (n % 5 == 0 && n % 3 != 0)
 		{
 			if (n == 100)
-			{
 				printf("Buzz");
-			}
 			else
-			{
 				printf("Buzz ");
-			}
 		}
-		else if (n % 3 != 0 || n % 5 != 0)
-		{
+		else if (n % 3 != 0 || n % 5 != 0;
 			printf("%d ", n);
-		}
 		else if (n % 3 == 0 && n % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 	}
 	printf("\n");
 	return (0);
